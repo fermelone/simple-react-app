441e0921f2393ca42e3817a35a3bf86d6401508e
Fern Melone <fermelone@gmail.com>
2024-02-08 14:09:15 +0000

    Changed logo to Gitpod's

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import gitpod_logo from './gitpod.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={gitpod_logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
